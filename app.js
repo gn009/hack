15822375e17273c7fad9500ea46cf4de1835154d
Jay Parmar <61630442+jay-2000@users.noreply.github.com>
2022-10-10 23:42:22 +0530

    added js for fiexed navbar

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ navToggle.addEventListener("click", function () {
   }
   // console.log(linksContainer.getBoundingClientRect());
 });
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
